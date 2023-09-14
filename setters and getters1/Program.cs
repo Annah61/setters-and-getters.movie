e71b6c85c7d0fa89f3fa2979da01d36e1b43c6ab
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-14 14:47:21 +0200

    Add project files.

---
+++ setters and getters1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace setters_and_getters1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            movie movie1 = new movie("Titanic", "Jim", "PG-13");
+            movie movie2 = new movie("The unreleased movie", "Evans", "Cat");
+            movie movie3 = new movie("The unrealeased movie", "Evans", "Cat");
+
+            Console.WriteLine(movie2.rating);
+            Console.WriteLine(movie.moviecount);
+            //freeze
+            Console.ReadLine();
+        }
+    }
+}
---
+++ setters and getters1/movie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace setters_and_getters1
+{
+    internal class movie
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+        // constructor
+        public movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating; // now movie rating is being set by the setter below
+
+        }
+                public string Rating
+            {
+
+                   get
+                   {
+                       return rating;
+
+                   } 
+                     set
+                     {
+                          if (value =="G" || value == "PG" || value =="PG-13" || value =="R")
+                          { rating = value; }
+
+                            else { rating = "NR"; }
+
+
+
+                     }   
+            }
+        
+            
+
+        
+    }
+}
